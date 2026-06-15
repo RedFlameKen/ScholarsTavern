5629085e9af3db235580fe425af49497a8a540c9
RedFlameKen <nettolacaba@gmail.com>
2026-06-15 16:48:03 +0800

    fix: remove comment

--- src/App.js
+++ src/App.js
@@ -4,31 +4,6 @@ import ChatPage from './components/pages/chat/ChatPage';
 import CallPage from './components/pages/call/CallPage';
 import LoginPage from './components/pages/login/LoginPage';
 
-// const sock = new WebSocket('ws://localhost:8000/chat/')
-//
-// sock.onopen = function() {
-//     console.log('websocket connection established, bruh')
-//
-//     const message = {
-//         'message': 'I\'ve connected'
-//     };
-//
-//     sock.send(JSON.stringify(message))
-// }
-//
-// sock.onmessage = function(response) {
-//     const message = JSON.parse(response.data)
-//     console.log(`message received: ${message['message']}`)
-// }
-//
-// function send_message(message){
-//     var message_json = {
-//         'message': message
-//     }
-//     sock.send(JSON.stringify(message_json))
-// }
-
-
 function App() {
   return (
       <BrowserRouter>
