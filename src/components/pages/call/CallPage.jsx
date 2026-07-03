6b7d5527339a3eac71ad65196efb9567f92e1035
RedFlameKen <nettolacaba@gmail.com>
2026-07-03 16:13:34 +0800

    fix: get the cameras to work both ends

--- src/components/pages/call/CallPage.jsx
+++ src/components/pages/call/CallPage.jsx
@@ -1,107 +1,133 @@
-const sock = new WebSocket("wss://scholarstavernserver.onrender.com/call/")
-
-const peer = new RTCPeerConnection({
-    iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
-})
+import { useEffect, useRef } from "react";
 
 let isCaller = false;
 
-async function handleOffer(data){
-    await peer.setRemoteDescription(new RTCSessionDescription(data.offer));
+function CallPage() {
+    const previewRef = useRef(null)
+    const remoteVidRef = useRef(null)
+    const sock = useRef(null)
+    const peer = useRef(null)
 
-    const answer = await peer.createAnswer();
-    await peer.setLocalDescription(answer);
+    // The play method is not allowed by the user agent or the platform in the current context, possibly because the user denied permission.
+    async function handleOffer(data) {
+        await peer.current.setRemoteDescription(new RTCSessionDescription(data.offer));
 
-    sock.send(JSON.stringify({
-        type: "answer",
-        answer
-    }))
-}
+        const answer = await peer.current.createAnswer();
+        await peer.current.setLocalDescription(answer);
 
-async function handleAnswer(data) {
-    await peer.setRemoteDescription(
-        new RTCSessionDescription(data.answer)
-    )
-}
-
-async function handleIceCandidate(data){
-    if(peer.remoteDescription){
-        await peer.addIceCandidate(
-            new RTCIceCandidate(data.candidate)
-        )
-    }
-}
-
-sock.onmessage = async (ev) => {
-    console.log(`raw message: ${ev.data}`)
-    if (ev.data == null)
-        return;
-
-    const data = JSON.parse(ev.data)
-    if (data.type === "offer" && !isCaller)
-        handleOffer(data)
-
-    if (data.type === "answer")
-        handleAnswer(data)
-
-    if (data.type === "ice-candidate")
-        handleIceCandidate(data)
-}
-
-peer.onicecandidate = (ev) => {
-    if (ev.candidate) {
-        sock.send(JSON.stringify({
-            type: "ice-candidate",
-            candidate: ev.candidate
+        sock.current.send(JSON.stringify({
+            type: "answer",
+            answer
         }))
     }
-}
 
-peer.ontrack = (ev) => {
-    // const audioEl = document.getElementById("remote_audio");
-    // audioEl.srcObject = new MediaStream(ev.streams[0].getAudioTracks())
-    // audioEl.play().catch(err => {
-    //     console.log(`autoplay blocked: ${err}`)
-    // })
+    async function handleAnswer(data) {
+        await peer.current.setRemoteDescription(
+            new RTCSessionDescription(data.answer)
+        )
+    }
 
-    const videoEl = document.getElementById("remote_video");
-    videoEl.srcObject = ev.streams[0]
-    videoEl.play().catch(err => {
-        console.log(`autoplay blocked: ${err}`)
-    })
-}
+    async function handleIceCandidate(data) {
+        if (peer.current.remoteDescription) {
+            await peer.current.addIceCandidate(
+                new RTCIceCandidate(data.candidate)
+            )
+        }
+    }
 
-async function startCall(){
-    isCaller = true;
-    const call_stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-    });
+    async function startCall() {
+        isCaller = true;
+        const call_stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
 
-    call_stream.getTracks().forEach(track => {
-        peer.addTrack(track, call_stream)
-    })
-    const offer = await peer.createOffer();
-    await peer.setLocalDescription(offer);
-    sock.send(JSON.stringify({
-        type: "offer",
-        offer
-    }))
-}
+        if (previewRef.current) {
+            previewRef.current.srcObject = call_stream
+            previewRef.current.onloadedmetadata = () => {
+                previewRef.current.play()
+            }
+        }
+
+        call_stream.getTracks().forEach(track => {
+            peer.current.addTrack(track, call_stream)
+            console.log(peer.current.signalingState)
+            console.log(peer.current.getSenders())
+        })
+        const offer = await peer.current.createOffer();
+        await peer.current.setLocalDescription(offer);
+        sock.current.send(JSON.stringify({
+            type: "offer",
+            offer
+        }))
+    }
+
+    useEffect(() => {
+        sock.current = new WebSocket("wss://scholarstavernserver.onrender.com/call/")
+        peer.current = new RTCPeerConnection({
+            iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+        })
+
+        sock.current.onmessage = async (ev) => {
+            // console.log(`raw message: ${ev.data}`)
+            if (ev.data == null)
+                return;
+
+            const data = JSON.parse(ev.data)
+            if (data.type === "offer")
+                handleOffer(data)
+
+            if (data.type === "answer")
+                handleAnswer(data)
+
+            if (data.type === "ice-candidate")
+                handleIceCandidate(data)
+        }
+
+        peer.current.onicecandidate = (ev) => {
+            if (ev.candidate) {
+                sock.current.send(JSON.stringify({
+                    type: "ice-candidate",
+                    candidate: ev.candidate
+                }))
+            }
+        }
+
+        peer.current.ontrack = (ev) => {
+            // const audioEl = document.getElementById("remote_audio");
+            // audioEl.srcObject = new MediaStream(ev.streams[0].getAudioTracks())
+            // audioEl.play().catch(err => {
+            //     console.log(`autoplay blocked: ${err}`)
+            // })
+
+            if (remoteVidRef.current) {
+                remoteVidRef.current.srcObject = ev.streams[0]
+                remoteVidRef.current.onloadedmetadata = () => {
+                    // remoteVidRef.current.play()
+                }
+            }
+        }
+
+        return () => {
+            sock.current?.close();
+            peer.current?.close();
+        }
+    }, [])
 
 
-function CallPage() {
-  return (
-    <div className="App">
-      Breh, this is the call page
-      <button type="button" onClick={_ => startCall()}>Start Call</button>
-      <br/>
-      {/* <audio id="remote_audio" controls autoPlay> */}
-      {/* </audio> */}
-      <video id="remote_video" width="200" height="180" controls>
-      </video>
-    </div>
-  );
+    return (
+        <div className="App">
+            Breh, this is the call page
+            <button type="button" onClick={_ => startCall()}>Start Call</button>
+            <br />
+            {/* <audio id="remote_audio" controls autoPlay> */}
+            {/* </audio> */}
+            <video ref={previewRef} id="self_video" width="200" height="180" controls>
+            </video>
+            <video ref={remoteVidRef} width="200" height="180" controls>
+            </video>
+        </div>
+    );
 }
 
 export default CallPage;
