744780434cd6c1d33b5cdd78f7263e3cf23134cb
RedFlameKen <nettolacaba@gmail.com>
2026-07-27 16:08:59 +0800

    fix: revert to old NavBarTop

--- src/components/nav_bar_top/NavBarTop.jsx
+++ src/components/nav_bar_top/NavBarTop.jsx
@@ -4,21 +4,14 @@ import "./NavBarTop.css";
 
 /**
 */
-function NavBarTop({buttons, mobileCompact = false, hideButtonOnMobile = null}){
-    const hideIndex = typeof hideButtonOnMobile === "number" ? hideButtonOnMobile : null;
-
+function NavBarTop({buttons}){
     return (
         <div id="nav_bar_top">
             <img src={logo} alt="Scholar's Tavern"/>
             <h1 id="nav_bar_top_header">Scholar's Tavern</h1>
             <div id="nav_bar_top_button_row">
                 { buttons.map((item, i) => {
-                    const hideOnMobile = (mobileCompact && i === 0) || (hideIndex !== null && i === hideIndex);
-                    return (
-                        <span key={i} className={hideOnMobile ? "nav-bar-top-button hide-on-mobile" : "nav-bar-top-button"}>
-                            {item}
-                        </span>
-                    )
+                    return (<span key={i}>{item}</span>)
                 })}
             </div>
         </div>
